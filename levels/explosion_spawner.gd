0d32397e262bd2cd1a5aa2262e067e1e4b3bc912
Anthony Libardi <libardi42@gmail.com>
2024-12-12 20:27:28 -0500

    ground as scene

--- levels/explosion_spawner.gd
+++ levels/explosion_spawner.gd
@@ -5,11 +5,23 @@ extends Node2D
 @export var camera_reference: Camera2D
 
 
+@export var click: bool = true
+
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and click:
 		event = event as InputEventMouseButton
 		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 			var explosion = explosion_scene.instantiate() as Node2D
 			var cam_topleft = camera_reference.position - camera_reference.get_viewport_rect().size / 2
 			explosion.position = cam_topleft + event.position
+			add_child(explosion)
+
+
+func _process(_delta: float) -> void:
+	if not click:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			var explosion = explosion_scene.instantiate() as Node2D
+			var cam_topleft = camera_reference.position - camera_reference.get_viewport_rect().size / 2
+			explosion.position = cam_topleft + get_viewport().get_mouse_position()
 			add_child(explosion)
