df3df16530b49df40f7d9c62540a4c0f5a3f061d
Anthony Libardi <libardi42@gmail.com>
2024-12-14 23:45:58 -0500

    high and long jump based on input time

--- scenes/objects/slug.gd
+++ scenes/objects/slug.gd
@@ -1,13 +1,40 @@
 extends CharacterBody2D
 
 
-enum Facing { LEFT, RIGHT }
+@export var walk_speed := 50.0
+@export var long_jump_velocity := Vector2(200, -200)
+@export var high_jump_velocity := Vector2(50, -400)
+
+@onready var jump_timer: Timer = $JumpTimer
+
+var facing := 1.0
+var next_jump := Vector2.ZERO
+var going_to_jump := false
+var jump_now := false
 
 
-const SPEED := 50.0
-const JUMP_VELOCITY := -200.0
+func _ready():
+    jump_timer.timeout.connect(decide_jump)
 
-var facing := Facing.RIGHT
+
+func _input(event):
+    if is_on_floor():
+        if event.is_action_pressed("jump"):
+            going_to_jump = true
+            jump_timer.start()
+        elif event.is_action_released("jump"):
+            decide_jump()
+
+
+func decide_jump():
+    if going_to_jump and is_on_floor():
+        if jump_timer.is_stopped():
+            next_jump = high_jump_velocity
+        else:
+            next_jump = long_jump_velocity
+            jump_timer.stop()
+        jump_now = true
+    going_to_jump = false
 
 
 func _physics_process(delta: float) -> void:
@@ -15,16 +42,20 @@ func _physics_process(delta: float) -> void:
     if not is_on_floor():
         velocity += get_gravity() * delta
 
-    # Handle jump.
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
-
     # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction := Input.get_axis("move-left", "move-right")
-    if direction:
-        velocity.x = direction * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
+    if is_on_floor():
+        var move_direction := Input.get_axis("move-left", "move-right")
+        if move_direction and jump_timer.is_stopped():
+            facing = move_direction
+            velocity.x = move_direction * walk_speed
+        else:
+            velocity.x = move_toward(velocity.x, 0, walk_speed)
+
+    # Handle jump.
+    if jump_now and is_on_floor():
+        jump_now = false
+        velocity.y = next_jump.y
+        velocity.x = next_jump.x * facing
+        next_jump = Vector2.ZERO
 
     move_and_slide()
