2601f783fcef0c4a87c2488a0e959532dd84d918
Anthony Libardi <libardi42@gmail.com>
2024-12-14 21:06:01 -0500

    constant cut

---
+++ scenes/constant_cut.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+@onready var img: Polygon2D = $Polygon2D
+@onready var coll: CollisionPolygon2D = $CollisionPolygon2D
+
+
+func _process(_delta):
+    img.position = get_global_mouse_position()
+    coll.position = get_global_mouse_position()
+    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+        for body in get_overlapping_bodies():
+            if body is GroundQuadrant:
+                body.clip(coll.polygon, body.to_local(coll.global_position))
