d699bb0d088a50d7b97803c15c238e436caaa814
Anthony Libardi <libardi42@gmail.com>
2024-12-21 15:40:53 -0500

    remove old loader

--- scripts/debug_place.gd
+++ scripts/debug_place.gd
@@ -8,3 +8,4 @@ var player_ref: Slug
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("debug-place"):
         player_ref.position = CustomCamera.global_mouse_position()
+        player_ref.velocity = Vector2.ZERO
